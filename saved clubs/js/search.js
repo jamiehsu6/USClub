f397a8347a7eb08eb4427e1a1dea8b7fd06cfd9e
leilasapra <166571102+leilasapra@users.noreply.github.com>
2024-04-23 18:39:56 -0700

    search page

--- saved clubs/js/search.js
+++ saved clubs/js/search.js
@@ -1,36 +1,47 @@
-$(function(){
-const search = () =>{
-    const searchbox = document.getElementById("searchBar").value.toUpperCase();
-    const storeitems = document.getElementById("club-list")
-    const club = document.querySelectorAll(".club")
-    const cname = storeitems.getElementsByTagName("p")
-
-    for( var i=0; i < cname.length; i++){
-        let match = club[i].getElementsByTagName("club")[0];
-
-       if(match){
-         let textvalue= match.textContent || match.innerHTML
-
-         if(textvalue.toUpperCase().indexOf(searchbox) > -1){
-            club[i].style.display = "";
-         }else{
-            club[i].style.display = "none";
-         }
-       }
-    }
-};
-
-
-
-
-
-    
-
 
   
+   //const clublist = document.getElementById('#club-list');
+
+   //const loadclubs = async () => {
+      //const response = await fetch('/savedclubs/search');
+      //const clubs = await response.json();
+  // }
+
+
+//const search = () =>{
+   // const searchbox = document.getElementById("searchBar").value.toUpperCase();
+    //const storeitems = document.getElementById("club-list")
+    //const club = document.querySelectorAll(".club")
+    //const cname = storeitems.getElementsByTagName("name")
+
+    //let i = [ 
+      //"Trojan Marketing Group",
+      //"Women's Volleyball Club",
+      //"Innovative Design Club",
+      //"Red Cross Club",
+      //"Spark SC",
+      //"Black Student Alliance",
+    //]
 
 
 
-});
+   // for( var i=0; i < cname.length; i++){
+     //   let match = club[i].getElementsByTagName("club")[0];
+
+       //if(match){
+         //let textvalue= match.textContent || match.innerHTML
+
+         //if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+           // club[i].style.display = "";
+         //}else{
+           // club[i].style.display = "none";
+         //}
+       //}
+    //}
+//};
+
+
+
+
 
 
