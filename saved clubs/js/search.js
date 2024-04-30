6756f53916f189e269b351343e53550a42834f67
leilasapra <166571102+leilasapra@users.noreply.github.com>
2024-04-30 16:24:21 -0700

    search bar-real this time!

--- saved clubs/js/search.js
+++ saved clubs/js/search.js
@@ -1,44 +1,40 @@
-
+$(function(){
+ 
+  let allClubs = {
+   "Trojan Marketing Group":
+   $("#club1"),
+   "Women's Volleyball Club":
+    $("#club2"),
+   "Innovative Design Club":
+    $("#club3"),
+   "Red Cross Club":
+   $("#club4"),
+   "Spark SC":
+   $("#club5"),
+   "Black Student Alliance":
+   $("#club6")
+   }
+   
+    let clubList = $("#club-list");
+     for (let key in allClubs){
+     clubList.append($(allClubs[key]));
+   }
+     
+    $("#searchBar").keyup(function(){
+       let query =  $(this).val().toLowerCase();
+       clubList.children().hide();
+       for (let key in allClubs){
+        if(key.toLowerCase().startsWith(query)){
   
-   //const clublist = document.getElementById('#club-list');
-
-   //const loadclubs = async () => {
-      //const response = await fetch('/savedclubs/search');
-      //const clubs = await response.json();
-  // }
-
-
-//const search = () =>{
-   // const searchbox = document.getElementById("searchBar").value.toUpperCase();
-    //const storeitems = document.getElementById("club-list")
-    //const club = document.querySelectorAll(".club")
-    //const cname = storeitems.getElementsByTagName("name")
-
-    //let i = [ 
-      //"Trojan Marketing Group",
-      //"Women's Volleyball Club",
-      //"Innovative Design Club",
-      //"Red Cross Club",
-      //"Spark SC",
-      //"Black Student Alliance",
-    //]
-
-
-
-   // for( var i=0; i < cname.length; i++){
-     //   let match = club[i].getElementsByTagName("club")[0];
-
-       //if(match){
-         //let textvalue= match.textContent || match.innerHTML
-
-         //if(textvalue.toUpperCase().indexOf(searchbox) > -1){
-           // club[i].style.display = "";
-         //}else{
-           // club[i].style.display = "none";
-         //}
-       //}
-    //}
-//};
+        $(allClubs[key]).show();
+           
+          }
+       }
+ 
+ 
+    });
+ 
+}); 
 
 
 
--- savedclubs_checkbox/saved.js
+++ savedclubs_checkbox/saved.js
@@ -44,3 +44,38 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+$(function() {
+ 
+    let allClubs = {
+     "Trojan Marketing Group":
+     $("#club1"),
+     "Women's Volleyball Club":
+     $("#club2"),
+     "Innovative Design Club":
+     $("#club3"),
+     "Red Cross Club":
+     $("#club4"),
+     "Spark SC":
+     $("#club5"),
+     "Black Student Alliance":
+     $("#club6")
+     }
+     
+      let clubList = $("#club-list");
+       for (let key in allClubs){
+       clubList.append($(allClubs[key]));
+     }
+       
+      $("#searchBar").keyup(function(){
+         let query =  $(this).val().toLowerCase();
+         clubList.children().hide();
+         for (let key in allClubs){
+          if(key.toLowerCase().startsWith(query)){
+          $(allClubs[key]).show();
+             
+            }
+          }
+         });
+   
+    });
